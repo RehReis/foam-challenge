f5902e17c436f425a8e02d2199cd766440dcd908
Regina Grogan <regina.r.grogan@gmail.com>
2022-03-30 23:25:39 -0500

    Added pagination

--- client/src/components/Images.jsx
+++ client/src/components/Images.jsx
@@ -4,16 +4,11 @@ import axios from 'axios';
 
 function Images() {
   const [imgList, setImgList] = useState([]);
+  const [filter, setFilter] = useState(null);
+  const [page, setPage] = useState(0);
 
-  const getImages = (page, filter) => {
-    if (filter === 'foaming') {
-      filter = true;
-    } else if (filter === 'non-foaming') {
-      filter = false;
-    } else {
-      filter = null;
-    }
-
+  const getImages = () => {
+    console.log('Inside get', filter);
     axios
       .get('http://localhost:3070/', { params: {page, filter} })
       .then(({data}) => {
@@ -25,18 +20,39 @@ function Images() {
   }
 
   useEffect(() => {
-    getImages(0);
-  }, [])
+    getImages();
+  }, [filter, page])
 
+  const changePage = (num) => {
+    if (num === 1) {
+      setPage(page + 1);
+    } else {
+      setPage(page - 1);
+    }
+  }
+
+  const changeFilter = async (filterBy) => {
+    if (filterBy === 'foaming') {
+      await setFilter(true);
+    } else if (filterBy === 'non-foaming') {
+      await setFilter(false);
+    } else {
+      await setFilter(null);
+    }
+    setPage(0);
+  }
 
   return (
     <div>
       <label htmlFor="filter">Filter by:</label>
-      <select name="filter" id="filter" required="required" onChange={(e) => getImages(0, e.target.value)}>
+      <select name="filter" id="filter" required="required" onChange={(e) => changeFilter(e.target.value)}>
       <option value="unclassified">unclassified</option>
         <option value="foaming">foaming</option>
         <option value="non-foaming">non-foaming</option>
       </select>
+      <br></br>
+      {page > 0 && <button onClick={() => {changePage(-1)}}>Previous Page</button>}
+      {imgList.length > 11 && <button onClick={() => {changePage(1)}}>Next Page</button>}
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {imgList.map(img =>
         (<Image key={img['_id']} image={img} getImages={getImages}/>)
--- database/controllers.js
+++ database/controllers.js
@@ -11,7 +11,6 @@ module.exports.getClassification = (req, res) => {
       res.status(200).json(result);
     })
     .catch(err => {
-      console.log(err);
       res.sendStatus(500)
     })
 }
